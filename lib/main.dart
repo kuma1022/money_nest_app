a8ec0207f62da5f8cd80d29440e01539ba101838
Qian Xiong <qianxiong90@gmail.com>
2025-08-17 22:38:47 +0900

    update app name

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Money Nest',
+      title: 'MoneyGrow',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
